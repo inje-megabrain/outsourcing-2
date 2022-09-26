090958b8f0715d3dc3b4c35166f2e8647c250d4f
Younggun Park <ygpark0922@gmail.com>
2022-09-26 20:05:44 +0900

    feat: main page init

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import { Stack, Button } from 'react-bootstrap';
 
 const Main = () => (
-  <>
-  </>
-)
+  <div>
+    <Stack gap={2} style={{ padding: '20px', maxWidth: '700px' }}>
+      <h4 style={{ textAlign: 'left' }}>
+        환영합니다.
+        <br />본 프로그램은 선박 도장 훈련을 목적으로 설계되었습니다.
+      </h4>
+      <Button variant="primary" size="lg">
+        시작하기
+      </Button>
+    </Stack>
+  </div>
+);
 export default Main;
