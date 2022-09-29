ebb1d2fe76a80e04b243fa1fbd029ce0540f7f49
Younggun Park <ygpark0922@gmail.com>
2022-09-29 14:20:11 +0900

    chore: tailwindcss setup && pages init

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Stack, Button } from 'react-bootstrap';
 
 const Main = () => (
-  <div>
-    <Stack gap={2} style={{ padding: '20px', maxWidth: '700px' }}>
-      <h4 style={{ textAlign: 'left' }}>
-        환영합니다.
-        <br />본 프로그램은 선박 도장 훈련을 목적으로 설계되었습니다.
-      </h4>
-      <Button variant="primary" size="lg">
-        시작하기
-      </Button>
-    </Stack>
+  <div className="container mx-auto p-8">
+    <p className="text-xl">
+      환영합니다.
+      <br />본 프로그램은 선박 도장 훈련을 목적으로 설계되었습니다.
+    </p>
+    <button className="bg-sky-700 w-80 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">
+      시작하기
+    </button>
   </div>
 );
 export default Main;
