1d776e8e375b1d141a4099a2b3a51f5e18e97daa
Younggun Park <ygpark0922@gmail.com>
2022-10-01 02:04:20 +0900

    feat: main page like figma

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Logo from '../../assets/logo.svg';
 
 const Main = () => (
-  <div className="container mx-auto p-8">
-    <p className="text-xl">
-      환영합니다.
-      <br />본 프로그램은 선박 도장 훈련을 목적으로 설계되었습니다.
-    </p>
-    <button className="bg-sky-700 w-80 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">
-      시작하기
-    </button>
+  <div className="flex place-content-center items-center h-full w-full bg-[#F5F6F9]">
+    <div className="mx-auto justify-items-start">
+      <img src={Logo} />
+      <p className="text-xl text-center">
+        환영합니다.
+        <br />본 프로그램은 선박 도장 훈련을 목적으로 설계되었습니다.
+      </p>
+      <button className="bg-primary-blue mt-20 -rounded-lg w-full text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">
+        시작하기
+      </button>
+    </div>
   </div>
 );
 export default Main;
