eacbab3a4badc0ddbab3bd7f506ded19ca8a996a
Younggun Park <ygpark0922@gmail.com>
2022-10-02 00:49:56 +0900

    feat: add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 
-function App() {
-  return <Main />;
-}
+const App = () => (
+  <BrowserRouter>
+    <Route path="/" element={<Main />} />
+  </BrowserRouter>
+);
 
 export default App;
