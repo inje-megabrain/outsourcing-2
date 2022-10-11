5ad1b1a3c6ae388f0ce60d9b50b7b67b23d719f2
Younggun Park <ygpark0922@gmail.com>
2022-10-11 22:51:21 +0900

    feat: apply mail check verify api

--- src/pages/FindPW/VerifyEmail.tsx
+++ src/pages/FindPW/VerifyEmail.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Input, Button } from '../../components';
+import { Input, Button, ErrorMessage } from '../../components';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { mailcheckAPI } from '../../apis/auth';
 
-const VerifyEmail = () => {
+interface Props {
+  email: string;
+}
+type FormValues = {
+  code: string;
+};
+
+const formSchema = Yup.object()
+  .shape({
+    code: Yup.string()
+      .required('코드를 입력해주세요')
+      .matches(/^[0-9]+$/, '코드를 입력해주세요'),
+  })
+  .required();
+
+const VerifyEmail: React.FC<Props> = ({ email }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<FormValues>({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(formSchema),
+  });
   return (
-    <div className="grid grid-cols-3 gap-4 mt-2">
-      <Input type="text" placeholder="인증번호 입력" className="col-span-2"></Input>
-      <Button>재전송</Button>
-    </div>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await mailcheckAPI({ email: email, ...data });
+      })}
+    >
+      <div className="grid grid-cols-3 gap-4 mt-2">
+        <Input type="text" placeholder="인증번호 입력" className="col-span-2" />
+        <ErrorMessage>{errors.code?.message}</ErrorMessage>
+        <Button disabled={isSubmitting} type="submit">
+          재전송
+        </Button>
+      </div>
+    </form>
   );
 };
 
